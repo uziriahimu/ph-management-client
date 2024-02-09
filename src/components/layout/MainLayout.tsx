e95fdebd2f3ea1563031f0f9ce4a81906e01f9d5
uziriahimu <uziriahimu@gmail.com>
2024-02-09 19:14:46 +0600

    first commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,78 @@
+import { Layout, Menu, MenuProps } from "antd";
+
+import { Outlet } from "react-router-dom";
+const { Header, Content, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+    children: [
+      {
+        key: "4",
+        label: "Create Admin",
+      },
+      {
+        key: "4",
+        label: "Create Student",
+      },
+    ],
+  },
+];
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "4rem",
+          }}
+        >
+          <h1>PH University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet></Outlet>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AdminLayout from "../components/layout/AdminLayout";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import CreateStudent from "../pages/Admin/CreateStudent";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminDashboard />,
+      },
+      {
+        path: "createstudent",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
